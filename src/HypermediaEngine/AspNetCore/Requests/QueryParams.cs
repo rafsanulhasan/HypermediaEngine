ea29192d48b75f45da29c8f1acc06a32e3214e4a
Rafsanul Hasan <rafsanulhasan@outlook.com>
2026-04-28 20:13:14 +0600

    Add QueryParams record class for handling query parameters in requests

---
+++ src/HypermediaEngine/AspNetCore/Requests/QueryParams.cs
@@ -0,0 +1,21 @@
+﻿using HypermediaEngine.Requests.Paging;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace HypermediaEngine.Requests;
+
+public sealed record class QueryParams
+{
+    public QueryParams(QueryBody? body = null, OffsetOrCursorPaging? paging = null)
+    {
+        Body = body;
+        Paging = paging;
+    }
+
+    [FromBody]
+    public QueryBody? Body { get; set; }
+
+    //[JsonConverter(typeof(OneOfBaseJsonConverter))]
+    [FromQuery]
+    public OffsetOrCursorPaging? Paging { get; set; }
+}
