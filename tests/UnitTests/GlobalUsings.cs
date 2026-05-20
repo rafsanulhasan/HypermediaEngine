ac00c1d04656701af22c7270d974c2a8cfb2f4bd
Rafsanul Hasan <rafsanulhasan@outlook.com>
2026-05-20 06:49:42 +0600

    test: add FilterConditionOtherTests for null and object value handling

--- tests/UnitTests/GlobalUsings.cs
+++ tests/UnitTests/GlobalUsings.cs
@@ -1,13 +1,10 @@
 global using Bogus;
 
-global using Shouldly;
-
-global using NSubstitute;
-
 global using NUnit.Framework;
 
-global using System.Text.Json;
+global using Shouldly;
 
+global using System.Text.Json;
 using System.Diagnostics.CodeAnalysis;
 
 [assembly: ExcludeFromCodeCoverage]
